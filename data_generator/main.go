e7104982760c37109a6dbf992a4c8bb007561ff0
Arjun Prakash <arjunprakash027@gmail.com>
2025-12-22 19:53:16 +0000

    data generator using go

--- data_generator/main.go
+++ data_generator/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"encoding/csv"
+	"os"
+	"sync"
 )
 
 type expr struct {
@@ -11,33 +14,101 @@ type expr struct {
 	eval func(a, b, c int) int
 }
 
-var expressions = []expr{
-	{"%d + %d * %d = %d", func(a, b, c int) int { return a + b*c }},
-	{"%d * %d + %d = %d", func(a, b, c int) int { return a*b + c }},
-	{"%d - %d * %d = %d", func(a, b, c int) int { return a - b*c }},
-	{"%d * %d * %d = %d", func(a, b, c int) int { return a * b * c }},
-	{"%d + %d - %d = %d", func(a, b, c int) int { return a + b - c }},
-	{"%d - %d - %d = %d", func(a, b, c int) int { return a - b - c }},
-	{"%d + %d + %d = %d", func(a, b, c int) int { return a + b + c }},
-	{"%d * (%d + %d) = %d", func(a, b, c int) int { return a * (b + c) }},
-	{"(%d + %d) * %d = %d", func(a, b, c int) int { return (a + b) * c }},
+type row struct {
+	q string
+	a string
 }
 
-func generateSample(r *rand.Rand) string {
+var expressions = []expr{
+	{"%d + %d * %d", func(a, b, c int) int { return a + b*c }},
+	{"%d * %d + %d", func(a, b, c int) int { return a*b + c }},
+	{"%d - %d * %d", func(a, b, c int) int { return a - b*c }},
+	{"%d * %d * %d", func(a, b, c int) int { return a * b * c }},
+	{"%d + %d - %d", func(a, b, c int) int { return a + b - c }},
+	{"%d - %d - %d", func(a, b, c int) int { return a - b - c }},
+	{"%d + %d + %d", func(a, b, c int) int { return a + b + c }},
+	{"%d * (%d + %d)", func(a, b, c int) int { return a * (b + c) }},
+	{"(%d + %d) * %d", func(a, b, c int) int { return (a + b) * c }},
+}
+
+func generateSample(r *rand.Rand) (string, string) {
 	a := r.Intn(100)
 	b := r.Intn(100)
 	c := r.Intn(100)
 	
 	e := expressions[r.Intn(len(expressions))]
 	result := e.eval(a,b,c)
+	
+	question := fmt.Sprintf(e.format, a, b, c)
+	answer := fmt.Sprintf("%d", result)
 
-	return fmt.Sprintf(e.format, a, b, c, result)
+	return question, answer
 }
 
+func worker(
+	id int, 
+	jobs <-chan struct{},
+	out chan<- row,
+	wg *sync.WaitGroup,
+){
+	defer wg.Done()
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano() + int64(id)))
+
+	for range jobs {
+		q, a := generateSample(r)
+		out <- row{q, a}
+	}
+
+}
+
+
 func main() {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	sample := generateSample(r)
-	fmt.Println(sample)
+	
+	const (
+		numSamples = 10000
+		numWorkers = 8
+	)
+
+	file, err := os.Create("bodmas.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"question", "answer"})
+	
+	jobs := make(chan struct{}, 1000)
+	out := make(chan row, 1000)
+
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		go worker(i, jobs, out, &wg)
+	}
+
+	go func() {
+		for i := 0; i < numSamples; i++ {
+			jobs <- struct{}{}
+		}
+		close(jobs)
+	}()
+
+	go func(){
+		wg.Wait()
+		close(out)
+	}()
+
+	for r := range out {
+		writer.Write([]string{r.q, r.a})
+	}
+	
 }
 
 
