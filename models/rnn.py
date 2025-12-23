6b6f58917b667ae84c64ed85c12075a2b71785f3
Arjun Prakash <arjunprakash027@gmail.com>
2025-12-23 19:27:00 +0000

    started with basics of RNN

--- models/rnn.py
+++ models/rnn.py
@@ -55,7 +55,7 @@ def _(MAX_INPUT_LENGTH, MAX_OUTPUT_LEN):
 
         ids += [char_to_id["<PAD>"]] * (MAX_INPUT_LENGTH - len(ids))
         return ids
-    
+
     def encode_output(s: str) -> list:
         ids = [char_to_id["<SOS>"]]
         ids += [char_to_id[ch] for ch in s]
@@ -66,10 +66,88 @@ def _(MAX_INPUT_LENGTH, MAX_OUTPUT_LEN):
 
         ids += [char_to_id["<PAD>"]] * (MAX_OUTPUT_LEN - len(ids))
         return ids
-    
+
     def decode(ids: list) -> str:
         chars = [id_to_char[i] for i in ids if i != char_to_id["<PAD>"]]
         return "".join(chars).replace("<SOS>", "").replace("<EOS>", "")
+    return VOCAB, char_to_id, decode, encode_input, encode_output
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # Modelling goes here
+    """)
+    return
+
+
+@app.cell
+def _():
+    import torch
+    import torch.nn as nn
+    from torch.utils.data import Dataset, DataLoader
+    return DataLoader, Dataset, torch
+
+
+@app.cell
+def _(VOCAB):
+    VOCAB_SIZE = len(VOCAB)
+    EMB_DIM = 32
+    HIDDEN_DIM = 64
+    return
+
+
+@app.cell
+def _(Dataset, encode_input, encode_output, torch):
+    class ArthemeticDataset(Dataset):
+        def __init__(self, data):
+            self.data = data
+    
+        def __len__(self):
+            return len(self.data)
+    
+        def __getitem__(self, idx):
+            question, answer = self.data[idx]
+
+            answer = str(answer)
+            enc_ids = encode_input(question)
+            out_ids = encode_output(answer)
+
+            enc_inputs = torch.tensor(enc_ids, dtype=torch.long)
+            dec_inputs = torch.tensor(out_ids[:-1], dtype=torch.long)
+            dec_targets = torch.tensor(out_ids[1:], dtype=torch.long)
+
+            return enc_inputs, dec_inputs, dec_targets
+    return (ArthemeticDataset,)
+
+
+@app.cell
+def _(ArthemeticDataset, char_to_id, decode, df):
+    all_data = list(df.itertuples(index=False, name=None))
+
+    dataset = ArthemeticDataset(all_data)
+    enc, dec_in, dec_tgt = dataset[0]
+
+    print(enc.shape)
+    print(dec_in.shape)  
+    print(dec_tgt.shape) 
+
+    print(decode(enc.tolist()))
+    print(decode([char_to_id["<SOS>"]] + dec_tgt.tolist()))
+
+    return (dataset,)
+
+
+@app.cell
+def _(DataLoader, dataset):
+    loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    for enc_input, dec_input, dec_target in loader:
+        print(enc_input.shape)
+        print(dec_input.shape)
+        print(dec_target.shape)
+        break
+
     return
 
 
